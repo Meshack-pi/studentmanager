1076f43e7f27d4e0267054b79a7d0ab1d1e6e7a1
Meshack-pi <meshack.kiprono@strathmore.edu>
2025-10-28 15:49:03 +0300

    Model the user details eg username, password and the role

---
+++ src/main/java/com/example/studentmanager/model/User.java
@@ -0,0 +1,24 @@
+package com.example.studentmanager.model;
+
+public class User {
+    private String username;
+    private String password;
+    private String role; //this role can either be lecturer or student
+    
+
+    //User constructor
+    public User(String username,String password,String role){
+        this.username = username;
+        this.password = password;
+        this.role= role;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getRole(){
+        return role;
+    }
+}
