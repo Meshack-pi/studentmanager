905287bc2471be6b3456dbdba51425edab77f7e5
Meshack-pi <meshack.kiprono@strathmore.edu>
2025-10-28 15:47:53 +0300

    Add logout functionality to student view

---
+++ src/main/java/com/example/studentmanager/StudentController.java
@@ -0,0 +1,20 @@
+package com.example.studentmanager;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StudentController {
+    @FXML
+    public void logout(ActionEvent event) throws IOException{
+        Stage stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+}
