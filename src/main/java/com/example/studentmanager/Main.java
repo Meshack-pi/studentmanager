88c856c9f57402953a497ac4764915273aec702b
Meshack-pi <meshack.kiprono@strathmore.edu>
2025-10-28 13:56:30 +0300

    FXML File controller

---
+++ src/main/java/com/example/studentmanager/Main.java
@@ -0,0 +1,23 @@
+package com.example.studentmanager;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login_file2.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 560, 520);
+    stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
